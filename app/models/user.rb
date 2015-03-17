ec7e195a902a9083f468c7119baf03388971b61b
Ian Donovan <idonovan90@gmail.com>
2015-03-17 16:04:32 -0400

    Generates team alongside user entry

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@
 # email                   :string
 
 class User < ActiveRecord::Base
-  validates :email, presence: true
   belongs_to :team
   has_many :notes
+  validates :email, presence: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  before_create :assign_team
+
+  private
+
+  def assign_team
+    team_name = self.email.split("@").last.slice(/.*\./).chop
+    self.team = Team.first_or_create(name: team_name)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe User, type: :model do
   end
 
   describe "Invalid User" do
-    subject(:user) { User.new }
+    subject(:user) { User.new(email: "aaaaaaa") }
     it { is_expected.to_not be_valid }
+  end
+
+  describe "#assign_team" do
+    subject(:user) { create(:user, email: "ian@smashingboxes.com") }
+    it "properly assigns the user team" do
+     expect(user.team.name).to eq("smashingboxes")
+    end
   end
 end
